83c97c6b4b618c74ffba3d451a9fe8ae29296e87
callielamb <callielambourne@gmail.com>
2024-11-17 22:08:27 -0700

    still messing with it

--- src/AuthState.js
+++ src/AuthState.js
@@ -4,4 +4,8 @@ export const AuthState = {
     Authenticated: 'AUTHENTICATED',
     Unauthenticated: 'UNAUTHENTICATED',
   };
-  
+
+export const checkAuth = () => {
+    const token = localStorage.getItem('authToken');
+    return token ? AuthState.Authenticated : AuthState.Unauthenticated;
+};
